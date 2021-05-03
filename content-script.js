be4c40e69aa2e756716daaf40cec5ccb725a12fb
prashsaka <67445443+prashsaka@users.noreply.github.com>
2021-05-02 22:00:43 -0400

    Initial check in

---
+++ content-script.js
@@ -0,0 +1,10 @@
+
+setInterval(() => {
+  const pagelets = document.querySelectorAll('div[data-pagelet]');
+  for (let i = pagelets.length - 1; i >= 0; i--) {
+    const item = pagelets.item(i);
+    if (item.innerText.includes('Suggested for You')) {
+      item.remove();
+    }
+  }
+}, 1000);
