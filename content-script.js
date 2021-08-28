64bf1115be14e456215fd049f3410495d29a6f85
prashsaka <67445443+prashsaka@users.noreply.github.com>
2021-08-28 11:57:05 -0400

    Checking "suggested for you" case insensitive.

--- content-script.js
+++ content-script.js
@@ -3,7 +3,7 @@ setInterval(() => {
   const pagelets = document.querySelectorAll('div[data-pagelet]');
   for (let i = pagelets.length - 1; i >= 0; i--) {
     const item = pagelets.item(i);
-    if (item.innerText.includes('Suggested for You') || item.getElementsByClassName('sponsored_ad').length) {
+    if (item.innerText.toLowerCase().includes('suggested for you') || item.getElementsByClassName('sponsored_ad').length) {
       item.remove();
     }
   }
