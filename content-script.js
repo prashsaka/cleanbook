d6be48f960e9ee7653e67c117b3c45a118e7be7e
Prashanth Saka <psaka4@gmail.com>
2022-06-21 08:52:21 -0400

    Moving to version 3.

--- content-script.js
+++ content-script.js
@@ -3,7 +3,9 @@ setInterval(() => {
   const pagelets = document.querySelectorAll('div[data-pagelet]');
   for (let i = pagelets.length - 1; i >= 0; i--) {
     const item = pagelets.item(i);
-    if (item.innerText.toLowerCase().includes('suggested for you') || item.getElementsByClassName('sponsored_ad').length) {
+    if (item.innerText.toLowerCase().includes('suggested for you')
+      || item.innerText.toLowerCase().includes('reels and short')
+      || item.getElementsByClassName('sponsored_ad').length) {
       item.remove();
     }
   }
